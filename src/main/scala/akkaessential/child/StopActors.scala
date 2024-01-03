eeb475950e4a5e1ae6438b813a3bae8f3b5a1589
Andre Lucas <andrelucastic@gmail.com>
2024-01-03 01:32:29 +0000

    Stop actors

---
+++ src/main/scala/akkaessential/child/StopActors.scala
@@ -0,0 +1,73 @@
+package akkaessential.child
+
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
+import akka.actor.typed.scaladsl.Behaviors
+import akkaessential.child.StopActors.Child.Message
+import akkaessential.child.StopActors.Parent.{CreateChild, StopChild, TellChild}
+
+object StopActors {
+
+  object Parent {
+    trait Command
+    case class CreateChild(name: String) extends Command
+    case class TellChild(name: String, message: String) extends Command
+    case class StopChild(name: String) extends Command
+
+    def apply(): Behavior[Command] = createChild(Map())
+
+    def createChild(children: Map[String, ActorRef[Message]]):Behavior[Command] = Behaviors.receive{ (context, message) =>
+      message match
+        case CreateChild(name) =>
+          context.log.info(s"[parent] Creating a child with name $name")
+          val childRef: ActorRef[Message] = context.spawn(Child(), name)
+          createChild(children + (name -> childRef))
+
+        case TellChild(name, message) =>
+          children.get(name).fold(context.log.info(s"[parent] child $name could not be found"))(child => child ! Message(name, message))
+          Behaviors.same
+
+        case StopChild(name) =>
+          val childOption = children.get(name)
+           context.log.info(s"[parent] attempting to stop child with name $name")
+          childOption.fold(context.log.info(s"[parent] child $name could not be found"))(child => context.stop(child))
+          createChild(children - name)
+          //Behaviors.same
+    }
+
+  }
+
+  object Child {
+    case class Message(id: String, body: String)
+    def apply(): Behavior[Message] = receiveMessage()
+
+    def receiveMessage(): Behavior[Message] = Behaviors.receive{ (context, message) =>
+      context.log.info(s"[${context.self.path.name}] Hello, I'm ${message.id} and I received: ${message.body}")
+      Behaviors.same
+    }
+  }
+
+  def startGuardian(): Unit = {
+    val userGuardian: Behavior[Nothing] = Behaviors.setup { context =>
+      val parent = context.spawn(Parent(), "Parent")
+
+      parent ! CreateChild("andre")
+      parent ! CreateChild("lucas")
+
+      parent ! TellChild("andre", "hello")
+
+      parent ! StopChild("lucas")
+
+      parent ! TellChild("lucas", "Can you hear me ?")
+
+      Behaviors.empty
+    }
+
+    val system = ActorSystem(userGuardian, "system")
+    Thread.sleep(1000)
+    system.terminate()
+  }
+
+  def main(args: Array[String]): Unit = {
+    startGuardian()
+  }
+}
